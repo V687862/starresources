8596d53db345e96399260e232cbecaf16208d0d8
Vernon Leadbeater <vernon.leadbeater@gmail.com>
2023-10-06 03:04:57 -0400

    first commit

--- app.js
+++ app.js
@@ -11,34 +11,126 @@ class Good {
     }
 }
 
+const ISOCENTERED_MAGNET = 'Isocentered Magnet';
+const TAU_GRADE_RHEOSTAT = 'Tau Grade Rheostat';
+const AUSTENITIC_MANIFOLD = 'Austenitic Manifold';
+const ISOTOPIC_COOLANT = 'Isotopic Coolant';
+const MAG_PRESSURE_TANK = 'Mag Pressure Tank';
+const REACTIVE_GAUGE = 'Reactive Gauge';
+const ZERO_WIRE = 'Zero Wire';
+const COMM_RELAY = 'Comm Relay';
+const MONOPROPELLANT = 'Monopropellant';
+
+const COPPER = '(Cu) Copper';
+const COBALT = '(Co) Cobalt';
+const BERYLLIUM = '(Be) Beryllium';
+const SILVER = '(Ag) Silver';
+const ALUMINUM = '(Al) Aluminum';
+const NICKEL = '(Ni) Nickel';
+const IONIC_LIQUIDS = '(IL) Ionic Liquids';
+const TETRAFLUORIDES = '(XF4) Tetrafluorides';
+const ALKANES = '(HnCn) Alkanes';
+const IRON = '(Fe) Iron';
+const WATER = '(H2O) Water';
+const HELIUM = '(He) Helium';
+const LEAD = '(Pb) Lead';
+const CHLORINE = '(Cl) Chlorine';
+const ARGON = '(Ar) Argon';
+const URANIUM = '(U) Uranium';
+const CHLOROSILANES = '(SiH3Cl) Chlorosilanes';
+const IRIDIUM = '(Ir) Iridium';
+const TUNGSTEN = '(W) Tungsten';
+const FLUORINE = '(F) Fluorine';
+const BENZENES = '(C6H6) Benzenes';
+const TITANIUM = '(Ti) Titanium';
+const LITHIUM = '(Li) Lithium';
+const GOLD = '(Au) Gold';
+const TANTALUM = '(Ta) Tantalum';
+const PLATINUM = '(Pt) Platinum';
+const VANADIUM = '(V) Vanadium';
+const NEODYMIUM = '(Nd) Neodymium';
+const CARBOXYLIC_ACIDS = '(RCOOH) Carboxylic Acids';
+const MERCURY = '(Hg) Mercury';
+const CAESIUM = '(Cs) Caesium';
+const PALLADIUM = '(Pd) Palladium';
+const NEON = '(Ne) Neon';
+const EUROPIUM = '(Eu) Europium';
+const PLUTONIUM = '(Pu) Plutonium';
+const XENON = '(Xe) Xenon';
+const YTTERBIUM = '(Yb) Ytterbium';
+const ANTIMONY = '(Sb) Antimony';
+const ALDUMITE = '(Ad) Aldumite';
+const AQUEOUS_HEMATITE = '(H2FeO3) Aqueous Hematite';
+const CAELUMITE = '(Ca) Caelumite';
+const DYSPROSIUM = '(Dy) Dysprosium';
+const INDICITE = '(In) Indicite';
+const ROTHICITE = '(Rt) Rothicite';
+const TASINE = '(Ts) Tasine';
+const VERYL = '(Vy) Veryl';
+const VYTINIUM = '(Vt) Vytinium';
+
+
 const resources = {
-    copper: new Resource('copper'),
-    cobalt: new Resource('cobalt'),
-    beryllium: new Resource('beryllium'),
-    silver: new Resource('silver'),
-    aluminum: new Resource('aluminum'),
-    nickel: new Resource('nickel'),
-    ionic: new Resource('ionic liquids'),
-    tetraflourides: new Resource('tetraflourides'),
-    alkanes: new Resource('alkanes'),
-    iron: new Resource('iron')
+    [Resources.COPPER]: createResource(Resources.COPPER),
+    [Resources.COBALT]: createResource(Resources.COBALT),
+    [Resources.BERYLLIUM]: createResource(Resources.BERYLLIUM),
+    [Resources.SILVER]: createResource(Resources.SILVER),
+    [Resources.ALUMINUM]: createResource(Resources.ALUMINUM),
+    [Resources.NICKEL]: createResource(Resources.NICKEL),
+    [Resources.IONIC_LIQUIDS]: createResource(Resources.IONIC_LIQUIDS),
+    [Resources.TETRAFLUORIDES]: createResource(Resources.TETRAFLUORIDES),
+    [Resources.ALKANES]: createResource(Resources.ALKANES),
+    [Resources.IRON]: createResource(Resources.IRON),
+    [Resources.WATER]: createResource(Resources.WATER, true),
+    [Resources.HELIUM]: createResource(Resources.HELIUM, true),
+    [Resources.LEAD]: createResource(Resources.LEAD, true),
+    [Resources.CHLORINE]: createResource(Resources.CHLORINE, true),
+    [Resources.ARGON]: createResource(Resources.ARGON, true),
+    [Resources.URANIUM]: createResource(Resources.URANIUM, true),
+    [Resources.CHLOROSILANES]: createResource(Resources.CHLOROSILANES, true),
+    [Resources.IRIDIUM]: createResource(Resources.IRIDIUM, true),
+    [Resources.TUNGSTEN]: createResource(Resources.TUNGSTEN, true),
+    [Resources.FLUORINE]: createResource(Resources.FLUORINE, true),
+    [Resources.BENZENES]: createResource(Resources.BENZENES, true),
+    [Resources.TITANIUM]: createResource(Resources.TITANIUM, true),
+    [Resources.LITHIUM]: createResource(Resources.LITHIUM, true),
+    [Resources.GOLD]: createResource(Resources.GOLD, true),
+    [Resources.TANTALUM]: createResource(Resources.TANTALUM, true),
+    [Resources.PLATINUM]: createResource(Resources.PLATINUM, true),
+    [Resources.VANADIUM]: createResource(Resources.VANADIUM, true),
+    [Resources.NEODYMIUM]: createResource(Resources.NEODYMIUM, true),
+    [Resources.CARBOXYLIC_ACIDS]: createResource(Resources.CARBOXYLIC_ACIDS, true),
+    [Resources.MERCURY]: createResource(Resources.MERCURY, true),
+    [Resources.CAESIUM]: createResource(Resources.CAESIUM, true),
+    [Resources.PALLADIUM]: createResource(Resources.PALLADIUM, true),
+    [Resources.NEON]: createResource(Resources.NEON, true),
+    [Resources.EUROPIUM]: createResource(Resources.EUROPIUM, true),
+    [Resources.PLUTONIUM]: createResource(Resources.PLUTONIUM, true),
+    [Resources.XENON]: createResource(Resources.XENON, true),
+    [Resources.YTTERBIUM]: createResource(Resources.YTTERBIUM, true),
+    [Resources.ANTIMONY]: createResource(Resources.ANTIMONY, true),
+    [Resources.ALDUMITE]: createResource(Resources.ALDUMITE, true),
+    [Resources.AQUEOUS_HEMATITE]: createResource(Resources.AQUEOUS_HEMATITE, true),
+    [Resources.CAELUMITE]: createResource(Resources.CAELUMITE, true),
+    [Resources.DYSPROSIUM]: createResource(Resources.DYSPROSIUM, true),
+    [Resources.INDICITE]: createResource(Resources.INDICITE, true),
+    [Resources.ROTHICITE]: createResource(Resources.ROTHICITE, true),
+    [Resources.TASINE]: createResource(Resources.TASINE, true),
+    [Resources.VERYL]: createResource(Resources.VERYL, true),
+    [Resources.VYTINIUM]: createResource(Resources.VYTINIUM, true)
 };
 
-const goods = {};
-
-// Define goods that only depend on resources
-goods.magnet = new Good('Isocentered Magnet', [resources.copper, resources.cobalt]);
-goods.rheostat = new Good('Tau Grade Rheostat', [resources.copper, resources.beryllium]);
-goods.manifold = new Good('Austenitic Manifold', [resources.nickel, resources.iron]);
-goods.coolant = new Good('Isotopic Coolant', [resources.ionic, resources.tetraflourides]);
-goods.tank = new Good('Mag Pressure Tank', [resources.nickel, resources.aluminum]);
-goods.gauge = new Good('Reactive Gauge', [resources.aluminum, resources.copper]);
-goods.wire = new Good('Zero Wire', [resources.silver, resources.copper]);
-
-// Define goods that depend on other goods
-goods.relay = new Good('Comm Relay', [goods.magnet, goods.rheostat]);
-goods.monopropellant = new Good('Monopropellant', [goods.tank, resources.alkanes]);
-
+const goods = {
+    [Goods.ISOCENTERED_MAGNET]: createGood(Goods.ISOCENTERED_MAGNET, [Resources.COPPER, Resources.COBALT]),
+    [Goods.TAU_GRADE_RHEOSTAT]: createGood(Goods.TAU_GRADE_RHEOSTAT, [Resources.COPPER, Resources.BERYLLIUM]),
+    [Goods.AUSTENITIC_MANIFOLD]: createGood(Goods.AUSTENITIC_MANIFOLD, [Resources.NICKEL, Resources.IRON]),
+    [Goods.ISOTOPIC_COOLANT]: createGood(Goods.ISOTOPIC_COOLANT, [Resources.IONIC_LIQUIDS, Resources.TETRAFLUORIDES]),
+    [Goods.MAG_PRESSURE_TANK]: createGood(Goods.MAG_PRESSURE_TANK, [Resources.NICKEL, Resources.ALUMINUM]),
+    [Goods.REACTIVE_GAUGE]: createGood(Goods.REACTIVE_GAUGE, [Resources.ALUMINUM, Resources.COPPER]),
+    [Goods.ZERO_WIRE]: createGood(Goods.ZERO_WIRE, [Resources.SILVER, Resources.COPPER]),
+    [Goods.COMM_RELAY]: createGood(Goods.COMM_RELAY, [Goods.ISOCENTERED_MAGNET, Goods.TAU_GRADE_RHEOSTAT]),
+    [Goods.MONOPROPELLANT]: createGood(Goods.MONOPROPELLANT, [Goods.MAG_PRESSURE_TANK, Resources.ALKANES]),
+};
 let outpostCount = 0;
 
 function addOutpost() {
